1d4d67be9985c9b394c98521c59a72f61aedcaf8
candolfi <jcandolfi32@gmail.com>
2023-10-15 20:41:01 -0700

    Created base files for the project. Linked js and css files to the index.html file. Created first individual "square" in js. Will create full grid next.

---
+++ README.md
@@ -0,0 +1,2 @@
+This etch-a-sketch project will highlight the skills
+I have learnt in the Odin Project Foundations course
---
+++ javascript.js
@@ -0,0 +1,14 @@
+const container = document.querySelector(".container");
+
+const div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "red";
+
+// container.style.width = "100px";
+// container.style.height = "100px";
+// container.style.background = "green";
+// container.style.color = "green";
+
+container.appendChild(div);
