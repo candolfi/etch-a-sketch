922e93120a97b26ecf06e1117ce26b005a5f8e87
candolfi <jcandolfi32@gmail.com>
2023-10-16 07:10:58 -0700

    Fixed css so that divs take up entire container space

--- javascript.js
+++ javascript.js
@@ -4,14 +4,11 @@ let squares = 4;
 for(let i = 0; i < squares; i++){
 
     const row = document.createElement("div");
+    row.classList.add("row");
     for(let j = 0; j < squares; j++){
 
         const div = document.createElement("div");
         div.classList.add("square");
-        // div.style.width = "100px";
-        // div.style.height = "100px";
-        // div.style.border = "thick solid blue";
-        // div.style.background = "red";
         row.appendChild(div);
     }
 
